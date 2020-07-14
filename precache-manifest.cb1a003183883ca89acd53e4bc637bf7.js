b46187f919c08a2f545980a9a902ebe7e30c4efd
Anish <anish.saxenaa@gmail.com>
2020-07-14 15:49:18 +0530

    Updates

---
+++ precache-manifest.cb1a003183883ca89acd53e4bc637bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c250bcf334504b31c862639bc980a158",
+    "url": "/react-contactsmanager/index.html"
+  },
+  {
+    "revision": "eeb7d504c9059949cfdf",
+    "url": "/react-contactsmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "847ec6539fbaeb4248e1",
+    "url": "/react-contactsmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eeb7d504c9059949cfdf",
+    "url": "/react-contactsmanager/static/js/2.a19b2fd0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-contactsmanager/static/js/2.a19b2fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847ec6539fbaeb4248e1",
+    "url": "/react-contactsmanager/static/js/main.223182eb.chunk.js"
+  },
+  {
+    "revision": "4b27889acccdb52957a3",
+    "url": "/react-contactsmanager/static/js/runtime-main.d6856e87.js"
+  }
+]);
